91ba3f39b6f4e52ec5003e4f036a329166792596
sandrine-bd <sandrine.barrucand@le-campus-numerique.fr>
2025-11-04 16:30:22 +0100

    First commit

---
+++ src/main/java/com/eureka/server/ServerApplication.java
@@ -0,0 +1,25 @@
+package com.eureka.server;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+
+    public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
